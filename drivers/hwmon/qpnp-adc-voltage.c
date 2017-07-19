33767dd5a83967c81eec67c1e9a59854b1bdf816
Humberto Borba <humberos@gmail.com>
2017-07-19 20:51:31 -0300

    drivers: Fix most qcom direct references to HZ
    Change-Id: I2a5cabc7a94bd75bcb98e0338755bdb4c3dd1f2b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -104,7 +104,7 @@
 #define QPNP_VADC_CONV_TIMEOUT_ERR				2
 #define QPNP_VADC_CONV_TIME_MIN					1000
 #define QPNP_VADC_CONV_TIME_MAX					1100
-#define QPNP_ADC_COMPLETION_TIMEOUT				HZ
+#define QPNP_ADC_COMPLETION_TIMEOUT				1000
 #define QPNP_VADC_ERR_COUNT					20
 #define QPNP_OP_MODE_SHIFT					3
 
@@ -646,6 +646,7 @@ fail_unlock:
 }
 EXPORT_SYMBOL(qpnp_vadc_hc_read);
 
+>>>>>>> 4ee5175584b4... drivers: Fix most qcom direct references to HZ
 static int32_t qpnp_vadc_configure(struct qpnp_vadc_chip *vadc,
 			struct qpnp_adc_amux_properties *chan_prop)
 {
@@ -2000,7 +2001,7 @@ recalibrate:
 	} else {
 		rc = wait_for_completion_timeout(
 					&vadc->adc->adc_rslt_completion,
-					QPNP_ADC_COMPLETION_TIMEOUT);
+					msecs_to_jiffies(QPNP_ADC_COMPLETION_TIMEOUT));
 		if (!rc) {
 			rc = qpnp_vadc_read_reg(vadc, QPNP_VADC_STATUS1,
 							&status1, 1);
