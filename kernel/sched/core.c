5a2450aebfcd18599403d6786d6b82ca8d047adc
Nato66613 <normandija1945@gmail.com>
2017-11-15 22:17:02 +0200

    sched: Change default hmp values for msm8953

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1749,13 +1749,13 @@ __read_mostly unsigned int sysctl_sched_new_task_windows = 5;
  * For increase, send notification if
  *      freq_required - cur_freq > sysctl_sched_freq_inc_notify
  */
-__read_mostly int sysctl_sched_freq_inc_notify = 10 * 1024 * 1024; /* + 10GHz */
+__read_mostly int sysctl_sched_freq_inc_notify = 400000; /* for msm8953 */
 
 /*
  * For decrease, send notification if
  *      cur_freq - freq_required > sysctl_sched_freq_dec_notify
  */
-__read_mostly int sysctl_sched_freq_dec_notify = 10 * 1024 * 1024; /* - 10GHz */
+__read_mostly int sysctl_sched_freq_dec_notify = 400000; /* for msm8953 */
 
 static __read_mostly unsigned int sched_io_is_busy;
 
