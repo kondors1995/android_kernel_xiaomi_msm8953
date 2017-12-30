15cb05350b04b9bab3bce89f800b86b8295fdd6a
Nato66613 <normandija1945@gmail.com>
2017-12-30 12:25:32 +0200

    R6

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ kernel_dir=$PWD
 build=$kernel_dir/out
 export CROSS_COMPILE=~/Android/kernel/tc/aarch64-linux-android/bin/aarch64-cortex_a53-linux-android-
 kernel="REVOLT-O"
-version="R.4"
+version="R.6"
 vendor="xiaomi"
 device="mido"
 zip=zip
@@ -47,7 +47,7 @@ zip_name="$kernel"-"$version"-"$date"-"$device".zip
 
 # Kernel Details
 BASE_AK_VER="REVOLT-O"
-VER="R.4"
+VER="R.6"
 AK_VER="$BASE_AK_VER$VER"
 
 export KBUILD_BUILD_USER=NATO66613
