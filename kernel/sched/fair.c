derkling/linux

DEBUG: schedtune: add tracepoint for energy_diff() values
Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

kernel/sched/fair.c
@@ -5128,6 +5128,11 @@ static int energy_diff(struct energy_env *eenv)
 	eenv->energy_payoff = 0;
 
 	result = energy_diff_evaluate(eenv);
+	trace_sched_energy_diff(eenv->task,
+			eenv->src_cpu, eenv->dst_cpu, eenv->util_delta,
+			eenv->nrg.before, eenv->nrg.after, eenv->nrg.diff,
+			eenv->cap.before, eenv->cap.after, eenv->cap.delta,
+			eenv->nrg.delta, eenv->energy_payoff);
 
 	return result;
 }

kondors1995/android_kernel_xiaomi_msm8953

BUG=chrome-os-partner:44828
TEST=Boot kernel on Oak.
Change-Id: Id8fafbd85f6d81248f322e073ee790a7ceec0bf7

kernel/sched/fair.c
@@ -4728,6 +4728,7 @@ static int energy_diff(struct energy_env *eenv)
 	struct sched_domain *sd;
 	struct sched_group *sg;
 	int sd_cpu = -1, energy_before = 0, energy_after = 0;
+	int result;
 
 	struct energy_env eenv_before = {
 		.util_delta	= 0,
@@ -4771,7 +4772,15 @@ static int energy_diff(struct energy_env *eenv)
 	eenv->nrg.diff = eenv->nrg.after - eenv->nrg.before;
 	eenv->payoff = 0;
 
-	return energy_diff_evaluate(eenv);
+	result = energy_diff_evaluate(eenv);
+
+	trace_sched_energy_diff(eenv->task,
+			eenv->src_cpu, eenv->dst_cpu, eenv->util_delta,
+			eenv->nrg.before, eenv->nrg.after, eenv->nrg.diff,
+			eenv->cap.before, eenv->cap.after, eenv->cap.delta,
+			eenv->nrg.delta, eenv->payoff);
+
+	return result;
 }
 
 /*