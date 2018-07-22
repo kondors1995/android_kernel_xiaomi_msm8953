4fd13ea49ef01e713e485a7f37c443120dceee27
kondors1995 <normandija1945@gmail.com>
2018-07-22 15:50:25 +0300

    devfreq:Fix compilation

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -520,7 +520,7 @@ struct devfreq *devfreq_add_device(struct device *dev,
 						devfreq->profile->max_state *
 						devfreq->profile->max_state,
 						GFP_KERNEL);
-	devfreq->time_in_state = devm_kcalloc(&devfreq->dev, sizeof(unsigned long) *
+	devfreq->time_in_state = devm_kzalloc(&devfreq->dev, sizeof(unsigned long) *
 						devfreq->profile->max_state,
 						GFP_KERNEL);
 	devfreq->last_stat_updated = jiffies;
